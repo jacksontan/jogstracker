f2c7a98ca6b312e5f90c402d6f8ff6b7151c8881
Jackson Tan <jackson.tan.s@gmail.com>
2017-02-07 18:37:47 +0800

    jogstracker app init

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-//var dbURI = 'mongodb://localhost/joggersdb';
 var dbURI = 'mongodb://jacksontan:jacksontan@ds056789.mlab.com:56789/joggersdb'
-//var dbURI = process.env.PROD_MONGODB;
-//if (process.env.NODE_ENV === 'production') {
-//  dbURI = process.env.MONGOLAB_URI;
-//}
-console.log(dbURI);
+var dbURI = process.env.PROD_MONGODB;
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGOLAB_URI;
+}
 mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
